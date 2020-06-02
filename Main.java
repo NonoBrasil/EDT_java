fcff406041251192b0c62156e8594015226f5f24
Noémie Pasquier <44292207+NonoBrasil@users.noreply.github.com>
2020-06-02 17:12:06 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controleur;
+import Vue.*;
+/**
+ *
+ * @author Noémie
+ */
+public class Main {
+    public static void main(String[] args) {
+        Frame_connexion window = new Frame_connexion();
+        window.connexion_fond();
+    }
+}
