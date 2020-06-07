5de4d62e75b6a3ab96caef987b37aa5428a77f9e
Noémie Pasquier <44292207+NonoBrasil@users.noreply.github.com>
2020-06-07 22:05:33 +0200

    Add files via upload

---
+++ bdd (1).sql
@@ -0,0 +1,350 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  Dim 07 juin 2020 à 12:59
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `bdd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cours`
+--
+
+DROP TABLE IF EXISTS `cours`;
+CREATE TABLE IF NOT EXISTS `cours` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `cours`
+--
+
+INSERT INTO `cours` (`id`, `nom`) VALUES
+(1, 'Java'),
+(2, 'Maths'),
+(3, 'Sport'),
+(4, 'Droit criminel');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `enseignant`
+--
+
+DROP TABLE IF EXISTS `enseignant`;
+CREATE TABLE IF NOT EXISTS `enseignant` (
+  `id_utilisateur` int(11) NOT NULL,
+  `id_cours` int(11) NOT NULL,
+  PRIMARY KEY (`id_utilisateur`,`id_cours`),
+  KEY `fk_id_cours` (`id_cours`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `enseignant`
+--
+
+INSERT INTO `enseignant` (`id_utilisateur`, `id_cours`) VALUES
+(1, 1),
+(7, 3),
+(8, 2),
+(9, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etudiant`
+--
+
+DROP TABLE IF EXISTS `etudiant`;
+CREATE TABLE IF NOT EXISTS `etudiant` (
+  `id_utilisateur` int(11) NOT NULL,
+  `numero` int(11) DEFAULT NULL,
+  `id_groupe` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_utilisateur`),
+  KEY `id_groupe` (`id_groupe`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `etudiant`
+--
+
+INSERT INTO `etudiant` (`id_utilisateur`, `numero`, `id_groupe`) VALUES
+(2, 1198, 1),
+(5, 1501, 2),
+(6, 2496, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `groupe`
+--
+
+DROP TABLE IF EXISTS `groupe`;
+CREATE TABLE IF NOT EXISTS `groupe` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) DEFAULT NULL,
+  `id_promotion` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_promotion` (`id_promotion`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `groupe`
+--
+
+INSERT INTO `groupe` (`id`, `nom`, `id_promotion`) VALUES
+(2, 'TD6', 2022),
+(1, 'TD11', 2022),
+(3, 'TD1', 2021);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `promotion`
+--
+
+DROP TABLE IF EXISTS `promotion`;
+CREATE TABLE IF NOT EXISTS `promotion` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `promotion`
+--
+
+INSERT INTO `promotion` (`id`, `nom`) VALUES
+(2022, 'ING3'),
+(2021, 'ING4');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `salle`
+--
+
+DROP TABLE IF EXISTS `salle`;
+CREATE TABLE IF NOT EXISTS `salle` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) DEFAULT NULL,
+  `capacite` int(11) DEFAULT NULL,
+  `id_site` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_site` (`id_site`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `salle`
+--
+
+INSERT INTO `salle` (`id`, `nom`, `capacite`, `id_site`) VALUES
+(1, 'P345', 80, 2),
+(2, 'P445', 80, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `seance`
+--
+
+DROP TABLE IF EXISTS `seance`;
+CREATE TABLE IF NOT EXISTS `seance` (
+  `id` int(11) NOT NULL,
+  `semaine` int(11) DEFAULT NULL,
+  `jour` date DEFAULT NULL,
+  `heure_debut` time DEFAULT NULL,
+  `heure_fin` time DEFAULT NULL,
+  `etat` int(11) DEFAULT NULL,
+  `id_cours` int(11) DEFAULT NULL,
+  `id_type` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_cours` (`id_cours`),
+  KEY `id_type` (`id_type`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `seance`
+--
+
+INSERT INTO `seance` (`id`, `semaine`, `jour`, `heure_debut`, `heure_fin`, `etat`, `id_cours`, `id_type`) VALUES
+(1, 1, '2020-06-08', '08:30:00', '09:30:00', 2, 4, 2),
+(2, 1, '2020-06-11', '15:30:00', '16:30:00', 2, 3, 1),
+(3, 1, '2020-06-08', '12:30:00', '13:30:00', 2, 1, 3),
+(4, 1, '2020-06-09', '16:30:00', '17:30:00', 2, 2, 1),
+(5, 1, '2020-06-11', '10:30:00', '11:30:00', 2, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `seance_enseignants`
+--
+
+DROP TABLE IF EXISTS `seance_enseignants`;
+CREATE TABLE IF NOT EXISTS `seance_enseignants` (
+  `id_seance` int(11) NOT NULL,
+  `id_enseignant` int(11) NOT NULL,
+  PRIMARY KEY (`id_seance`,`id_enseignant`),
+  KEY `id_enseignant` (`id_enseignant`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `seance_enseignants`
+--
+
+INSERT INTO `seance_enseignants` (`id_seance`, `id_enseignant`) VALUES
+(1, 9),
+(2, 7),
+(3, 1),
+(4, 8),
+(5, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `seance_groupes`
+--
+
+DROP TABLE IF EXISTS `seance_groupes`;
+CREATE TABLE IF NOT EXISTS `seance_groupes` (
+  `id_seance` int(11) NOT NULL,
+  `id_groupe` int(11) NOT NULL,
+  PRIMARY KEY (`id_seance`,`id_groupe`),
+  KEY `id_groupe` (`id_groupe`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `seance_groupes`
+--
+
+INSERT INTO `seance_groupes` (`id_seance`, `id_groupe`) VALUES
+(1, 3),
+(2, 1),
+(3, 2),
+(4, 1),
+(5, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `seance_salles`
+--
+
+DROP TABLE IF EXISTS `seance_salles`;
+CREATE TABLE IF NOT EXISTS `seance_salles` (
+  `id_seance` int(11) NOT NULL,
+  `id_salle` int(11) NOT NULL,
+  PRIMARY KEY (`id_seance`,`id_salle`),
+  KEY `id_salle` (`id_salle`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `seance_salles`
+--
+
+INSERT INTO `seance_salles` (`id_seance`, `id_salle`) VALUES
+(1, 2),
+(2, 2),
+(3, 2),
+(4, 2),
+(5, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `site`
+--
+
+DROP TABLE IF EXISTS `site`;
+CREATE TABLE IF NOT EXISTS `site` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `site`
+--
+
+INSERT INTO `site` (`id`, `nom`) VALUES
+(1, 'Eiffel 1'),
+(2, 'Eiffel 2');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type_cours`
+--
+
+DROP TABLE IF EXISTS `type_cours`;
+CREATE TABLE IF NOT EXISTS `type_cours` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `type_cours`
+--
+
+INSERT INTO `type_cours` (`id`, `nom`) VALUES
+(1, 'TD'),
+(2, 'Cours margistral'),
+(3, 'TP');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `passwd` varchar(100) DEFAULT NULL,
+  `nom` varchar(100) DEFAULT NULL,
+  `prenom` varchar(100) DEFAULT NULL,
+  `droit` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id`, `email`, `passwd`, `nom`, `prenom`, `droit`) VALUES
+(1, 'segado@edu.ece.fr', 'jp', 'Segado', 'Jean Pierre', 3),
+(2, 'noemie.pasquier@edu.ece.fr', 'azerty', 'Pasquier', 'Noemie', 4),
+(3, 'paul.moquin@edu.ece.fr', 'dublin', 'Moquin', 'Paul', 1),
+(4, 'louis.fieux@edu.ece.fr', 'lovejava', 'Fieux', 'Louis', 2),
+(5, 'adpasquier@edu.ece.fr', 'brasil', 'Pasquier', 'Adrien', 4),
+(6, 'armelle@edu.ece.fr', 'montreal', 'Pasquier', 'Armelle', 4),
+(7, 'erisset@edu.ece.fr', 'judo', 'Erisset', 'Jacky', 3),
+(8, 'chaari@edu.ece.fr', 'proba', 'Chaari', 'Anis', 3),
+(9, 'fabienne.coudray@ece.fr', 'penal', 'Coudray', 'Fabienne', 3);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
